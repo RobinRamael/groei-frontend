49244b729f308d29a1fb49ca24e2fc55d87d2fe1
Robin Ramael <robin.ramael@maykinmedia.nl>
2019-07-03 15:43:58 +0200

    autoplay by default

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ function App({ history }) {
   let url = new URL(window.location);
   let searchParams = new URLSearchParams(url.search);
   let startAt = parseInt(searchParams.get("start") || "0");
-  let autoPlay = parseInt(searchParams.get("autoplay")) === 1;
+  let autoPlay = parseInt(searchParams.get("autoplay") || "1") === 1;
   let debug = parseInt(searchParams.get("debug") || "0") === 1;
 
   console.log(startAt, autoPlay);
